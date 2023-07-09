3f789fdcd3e26971ec44d99a578bda109ba2c86d
Cristian Manoliu <cristian.manoliu@gmail.com>
2023-07-09 22:46:55 +0300

    MergeStringsAlternately

---
+++ leetcode-75/src/main/java/cma/MergeStringsAlternately.java
@@ -0,0 +1,32 @@
+package cma;
+
+public class MergeStringsAlternately {
+  public String mergeAlternately(String word1, String word2) {
+    if (word1 == null) {
+      return word2;
+    }
+    if (word2 == null) {
+      return word1;
+    }
+    int lenWord1 = word1.length();
+    int lenWord2 = word2.length();
+    int min = Math.min(lenWord1, lenWord2);
+
+    StringBuilder result = new StringBuilder();
+    for (int index = 0; index < min; index++) {
+      result.append(word1.charAt(index));
+      result.append(word2.charAt(index));
+    }
+
+    if (lenWord1 > lenWord2) {
+      for (int index = min; index < lenWord1; index++) {
+        result.append(word1.charAt(index));
+      }
+    } else {
+      for (int index = min; index < lenWord2; index++) {
+        result.append(word2.charAt(index));
+      }
+    }
+    return result.toString();
+  }
+}
