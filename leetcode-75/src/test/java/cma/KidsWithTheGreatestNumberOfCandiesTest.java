f1590bf77afeb95efd5f002dfb2ee300429560d8
Cristian Manoliu <cristian.manoliu@gmail.com>
2023-07-10 17:00:02 +0300

    KidsWithTheGreatestNumberOfCandies

---
+++ leetcode-75/src/test/java/cma/KidsWithTheGreatestNumberOfCandiesTest.java
@@ -0,0 +1,19 @@
+package cma;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class KidsWithTheGreatestNumberOfCandiesTest {
+
+  private final KidsWithTheGreatestNumberOfCandies underTest =
+      new KidsWithTheGreatestNumberOfCandies();
+
+  @Test
+  public void kidsWithCandies() {
+    assertThat(underTest.kidsWithCandies(new int[] {2, 3, 5, 1, 3}, 3))
+        .isEqualTo(List.of(Boolean.TRUE, Boolean.TRUE, Boolean.TRUE, Boolean.FALSE, Boolean.TRUE));
+  }
+}
