1e5ae2ceb831dffcb7f76abdd394ecdd560277f1
Cristian Manoliu <cristian.manoliu@gmail.com>
2023-07-10 13:04:20 +0300

    GreatestCommonDivisorOfStrings

---
+++ leetcode-75/src/test/java/cma/GreatestCommonDivisorOfStringsTest.java
@@ -0,0 +1,24 @@
+package cma;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class GreatestCommonDivisorOfStringsTest {
+
+  private final GreatestCommonDivisorOfStrings underTest = new GreatestCommonDivisorOfStrings();
+
+  @Test
+  void gcdOfStrings() {
+    assertThat(underTest.gcdOfStrings("ABCABC", "ABC"))
+        .as("GCDOS between ABCABC and ABC is ABC")
+        .isEqualTo("ABC");
+    assertThat(underTest.gcdOfStrings("ABABAB", "ABAB"))
+        .as("GCDOS between ABABAB and ABAB is ABAB")
+        .isEqualTo("AB");
+    assertThat(underTest.gcdOfStrings("LEET", "CODE"))
+        .as("GCDOS between LEET and CODE is empty string")
+        .isEqualTo("");
+  }
+}
