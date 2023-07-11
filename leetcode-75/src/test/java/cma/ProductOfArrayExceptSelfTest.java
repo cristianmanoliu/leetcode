80ad7b50cdb034269d13172301bdcb6cf24a8e31
Cristian Manoliu <cristian.manoliu@gmail.com>
2023-07-11 20:30:03 +0300

    ProductOfArrayExceptSelf

---
+++ leetcode-75/src/test/java/cma/ProductOfArrayExceptSelfTest.java
@@ -0,0 +1,17 @@
+package cma;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ProductOfArrayExceptSelfTest {
+
+  private final ProductOfArrayExceptSelf underTest = new ProductOfArrayExceptSelf();
+
+  @Test
+  public void productExceptSelf() {
+    assertThat(underTest.productExceptSelf(new int[]{1, 2, 3, 4}))
+        .isEqualTo(new int[]{24, 12, 8, 6});
+  }
+
+}
