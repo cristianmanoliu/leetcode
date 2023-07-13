13e3bdc2e239e87e7a9db00c7300be478febcd7f
Cristian Manoliu <cristian.manoliu@gmail.com>
2023-07-13 23:57:07 +0300

    IsSubsequence

---
+++ leetcode-75/src/test/java/cma/IsSubsequenceTest.java
@@ -0,0 +1,18 @@
+package cma;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class IsSubsequenceTest {
+
+  private final IsSubsequence underTest = new IsSubsequence();
+
+  @Test
+  void isSubsequence() {
+
+    assertThat(underTest.isSubsequence("aaaaaa", "bbaaaa")).describedAs("aaaaaa in bbaaaa").isFalse();
+    assertThat(underTest.isSubsequence("abc", "ahbgdc")).describedAs("abc in ahbgdc").isTrue();
+    assertThat(underTest.isSubsequence("axc", "ahbgdc")).describedAs("axc in ahbgdc").isFalse();
+  }
+}
