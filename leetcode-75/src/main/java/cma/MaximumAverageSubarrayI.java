81806fc1cb5a1ce89e7a8f2909db6eee666b037c
Cristian Manoliu <cristian.manoliu@gmail.com>
2025-04-12 00:12:55 +0300

    MaximumAverageSubarrayI

---
+++ leetcode-75/src/main/java/cma/MaximumAverageSubarrayI.java
@@ -0,0 +1,18 @@
+package cma;
+
+public class MaximumAverageSubarrayI {
+    public double findMaxAverage(int[] nums, int k) {
+        double sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        double maxSum = sum;
+
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, sum);
+        }
+        return maxSum / k;
+    }
+}
+
