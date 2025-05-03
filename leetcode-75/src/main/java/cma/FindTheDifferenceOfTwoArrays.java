c09e9cd92e520cfc5c4057d0b2d880dd444e74b7
Cristian Manoliu <cristian.manoliu@gmail.com>
2025-05-03 20:39:45 +0300

    Add FindTheDifferenceOfTwoArrays

---
+++ leetcode-75/src/main/java/cma/FindTheDifferenceOfTwoArrays.java
@@ -0,0 +1,38 @@
+package cma;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class FindTheDifferenceOfTwoArrays {
+
+  public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+    Set<Integer> set1 = new HashSet<>();
+    for (int num : nums1) {
+      set1.add(num);
+    }
+    Set<Integer> set2 = new HashSet<>();
+    for (int num : nums2) {
+      set2.add(num);
+    }
+
+    List<Integer> onlyIn1 = new ArrayList<>();
+    List<Integer> onlyIn2 = new ArrayList<>();
+
+    for (int num : set1) {
+      if (!set2.contains(num)) {
+        onlyIn1.add(num);
+      }
+    }
+
+    for (int num : set2) {
+      if (!set1.contains(num)) {
+        onlyIn2.add(num);
+      }
+    }
+
+    return Arrays.asList(onlyIn1, onlyIn2);
+  }
+}
---
+++ leetcode-75/src/test/java/cma/FindTheDifferenceOfTwoArraysTest.java
@@ -0,0 +1,22 @@
+package cma;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class FindTheDifferenceOfTwoArraysTest {
+
+  private final FindTheDifferenceOfTwoArrays underTest = new FindTheDifferenceOfTwoArrays();
+
+  @Test
+  public void findDifference() {
+    int[] nums1 = {1, 2, 3, 3};
+    int[] nums2 = {2, 4, 4};
+    List<List<Integer>> result = underTest.findDifference(nums1, nums2);
+
+    assertTrue(result.get(0).containsAll(Arrays.asList(1, 3)) && result.get(0).size() == 2);
+    assertTrue(result.get(1).contains(4) && result.get(1).size() == 1);
+  }
+}
